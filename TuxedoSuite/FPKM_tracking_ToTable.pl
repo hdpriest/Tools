4af5630d4a0765d9037c70dcfcbbec5b8914c783
EC2 Default User <ec2-user@ip-172-31-23-187.ec2.internal>
2014-05-06 23:22:01 +0000

    lib correction

--- TuxedoSuite/FPKM_tracking_ToTable.pl
+++ TuxedoSuite/FPKM_tracking_ToTable.pl
@@ -1,7 +1,8 @@
 #!/usr/bin/perl
 use warnings;
 use strict;
-
+use FindBin;
+use lib "$FindBin::Bin/../Lib";
 use hdpTools;
 
 die "usage: perl $0 <input FPKM tracking file> <output table> <min FPKM to report> <Ordered list of BAM files\n\n" unless $#ARGV>=1;
